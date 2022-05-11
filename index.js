c45a995e3ad2f41736df30aa51357c48f908be66
lisandradecassia <lisandradecassia@gmail.com>
2022-05-11 18:33:17 -0300

    Apliquei o JS para interação das páginas

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-//Mudar de página ao clicar
 function comecar() {
     location.assign("./paginaApod/paginaApod.html");
 }
@@ -7,4 +5,25 @@ function inicio() {
     location.assign("../index.html");
 }
 
-//
+$('#submit').click(function () {
+    const selecionarData = $('#inputData').val()
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=eAZxypDpacwuoidmVhHlCHZyB0TV0zUJ3mYRknyP&date=${selecionarData}`,
+        type: 'GET',
+        success: function (retorno) {
+            $('#tituloApod').text(retorno.title);
+            $('#textApod').text(retorno.explanation);
+            if (retorno.media_type == 'image') {
+                $('#imgApod').attr('src', retorno.url);
+                $('#videoApod').hide();
+                $('#imgApod').show();
+            } else {
+                $('#videoApod').attr('src', retorno.url);
+                $('#videoApod').show()
+                $('#imgApod').hide();
+            }
+        }
+    })
+})
+$('#videoApod').hide();
+$('#imgApod').hide();
