53e02d0ee608df3d70e08f583e55f3dda0ee74f4
lisandradecassia <lisandradecassia@gmail.com>
2022-05-12 14:58:26 -0300

    Limitando data no input de data em JS.

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ function comecar() {
 function inicio() {
     location.assign("../index.html");
 }
-
+$('#inputData').attr('max', new Date().toISOString().split("T")[0]);
 $('#submit').click(function () {
     const selecionarData = $('#inputData').val()
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=eAZxypDpacwuoidmVhHlCHZyB0TV0zUJ3mYRknyP&date=${selecionarData}`,
         type: 'GET',
+
         success: function (retorno) {
-            $('#tituloApod').text(retorno.title);
-            $('#textApod').text(retorno.explanation);
+
             if (retorno.media_type == 'image') {
+                $('#tituloApod').text(retorno.title);
+                $('#textApod').text(retorno.explanation);
                 $('#imgApod').attr('src', retorno.url);
                 $('#videoApod').hide();
                 $('#imgApod').show();
+
             } else {
                 $('#videoApod').attr('src', retorno.url);
                 $('#videoApod').show()
@@ -25,5 +28,6 @@ $('#submit').click(function () {
         }
     })
 })
+
 $('#videoApod').hide();
 $('#imgApod').hide();
